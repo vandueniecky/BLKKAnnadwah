5925cc0bad22a566c13247e021463406a57a8629
vandueniecky <vandu.niecky14@gmail.com>
2021-11-11 20:30:41 +0700

    update Contact 0.11.11.21

--- script.js
+++ script.js
@@ -47,7 +47,33 @@ $('.close-trigger').click(function() {
     $('span.navbar-toggler-icon').toggleClass('close');
 });
 
+// Contact 
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbyX4x7yxDJyNM3vx3YRpHz1BiC_5p87bE3k8EipBX9o9XtD7pnESR6h3hGppl237vDa/exec'
+const form = document.forms['contact-from-webiste'];
+const btnKirim = document.querySelector('.btn-kirim');
+const btnLoading = document.querySelector('.btn-loading');
+const myAlert = document.querySelector('.my-alert');
 
+form.addEventListener('submit', e => {
+  e.preventDefault();
+        // ketika tombol sumbit diklik
+        // tampilkan tombol loading dan hilangkan tombol kirim
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
 
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        // setelah tombol sumbit diklik
+        // tampilkan tombol kirim dan hilangkan loading
+        btnLoading.classList.toggle('d-none');
+        btnKirim.classList.toggle('d-none');
+        // tampilkan alert pesan success 
+        myAlert.classList.toggle('d-none');
+        // reset form nya
+        form.reset();
 
+        console.log('Success!', response);
+    })
+    .catch(error => console.error('Error!', error.message))
+});
